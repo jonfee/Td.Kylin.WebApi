e47ea8c14f5a1bf599843fce91dec361659110ff
明月 <hibruin@outlook.com>
2015-11-11 18:19:31 +0800

    创建项目和测试的webapi

---
+++ src/Td.Kylin.WebApi/Filters/ApiAuthorizationAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Filters;
+
+namespace Td.Kylin.WebApi.Filters
+{
+    public class ApiAuthorizationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var request = context.HttpContext.Request;
+            var method = request.Method;
+            var query = request.QueryString.Value.Trim(new char[] { '?', ' ' });
+
+
+        }
+    }
+}
