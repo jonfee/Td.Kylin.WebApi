796a66b6cb72e6c2a2b09fa8b9e4f3fdcdb6b788
伍朝辉 <337883612@qq.com>
2015-12-22 14:07:32 +0800

    新增文件上传

---
+++ src/Td.Kylin.WebApi/FileUpload/Extensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Td.AspNet.Upload;
+
+namespace Td.Kylin.WebApi.FileUpload
+{
+    /// <summary>
+    /// 上传扩展类
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// 获取最终文件上传的文件夹路径
+        /// </summary>
+        /// <param name="uploadBoot">文件上传的根（如：upload）</param>
+        /// <param name="uploadFolder">从上传根后指定的文件夹路径部分（如：“mall/product/”，不存在时自动存储在other文件夹）</param>
+        /// <param name="needDate">是否需要年月日作为路径的一部分（如：15/12/22）</param>
+        /// <returns></returns>
+        public static string GetUploadFolder(this string uploadFolder,string uploadBoot,  bool needDate = false)
+        {
+            string path = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(uploadFolder))
+            {
+                uploadFolder = "other";
+            }
+
+            path = Path.Combine(uploadBoot, uploadFolder);
+
+            if (needDate)
+            {
+                path = Path.Combine(path, DateTime.Now.ToString("yy/MM/dd"));
+            }
+
+            return path;
+        }
+
+        /// <summary>
+        /// 转换上传后的文件结果
+        /// </summary>
+        /// <param name="result">上传后的结果集</param>
+        /// /// <param name="startBoot">文件路径起始字符</param>
+        /// <param name="startReplace">文件路径起始字符要替换的字符</param>
+        public static void ConvertUploadResult(this IEnumerable<UploadResult> result, string startBoot, string startReplace)
+        {
+            if (string.IsNullOrWhiteSpace(startBoot)) return;
+
+            if (string.IsNullOrWhiteSpace(startReplace)) return;
+
+            string boot = startBoot;
+            if (!boot.EndsWith("/")) boot = boot + "/";
+
+            string replace = startReplace;
+            if (!replace.EndsWith("/")) replace = replace + "/";
+
+            if (null != result && result.Count() > 0)
+            {
+                foreach (var rst in result)
+                {
+                    if (rst.FilePath.TrimStart('/').StartsWith(boot, StringComparison.OrdinalIgnoreCase))
+                    {
+                        rst.FilePath = replace + rst.FilePath.Remove(0, boot.Length);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 转换上传后文件结果
+        /// </summary>
+        /// <param name="result">上传结果</param>
+        /// <param name="startBoot">文件路径起始字符</param>
+        /// <param name="startReplace">文件路径起始字符要替换的字符</param>
+        public static void ConvertUploadResult(this UploadResult result, string startBoot, string startReplace)
+        {
+            if (null != result)
+            {
+                string boot = startBoot;
+                if (!boot.EndsWith("/")) boot = boot + "/";
+
+                string replace = startReplace;
+                if (!replace.EndsWith("/")) replace = replace + "/";
+
+                if (result.FilePath.TrimStart('/').StartsWith(boot, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.FilePath = replace + result.FilePath.Remove(0, boot.Length);
+                }
+            }
+        }
+    }
+}
---
+++ src/Td.Kylin.WebApi/FileUpload/KylinUploader.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Td.AspNet.Upload;
+
+namespace Td.Kylin.WebApi.FileUpload
+{
+    /// <summary>
+    /// Kylin文件上传器
+    /// </summary>
+    public class KylinUploader
+    {
+        /// <summary>
+        /// 文件上传器初始化实例
+        /// </summary>
+        /// <param name="context">文件上传上下文实例</param>
+        /// <param name="startBoot">上传后文件根路径
+        /// <para>如：文件路径为 upload/product/2312.gif，根路径为“upload”</para>
+        /// </param>
+        /// <param name="startReplace">上传后文件根路径替换字符串
+        /// <para>如：mall，即将“upload/product/2312.gif”替换成为“mall/product/2312.gif”</para>
+        /// </param>
+        public KylinUploader(UploadContext context, string startBoot, string startReplace) : this(new[] { context }, startBoot, startReplace) { }
+
+        /// <summary>
+        /// 文件上传器初始化实例
+        /// </summary>
+        /// <param name="context">文件上传上下文实例集合</param>
+        /// <param name="startBoot">上传后文件根路径
+        /// <para>如：文件路径为 upload/product/2312.gif，根路径为“upload”</para>
+        /// </param>
+        /// <param name="startReplace">上传后文件根路径替换字符串
+        /// <para>如：mall，即将“upload/product/2312.gif”替换成为“mall/product/2312.gif”</para>
+        /// </param>
+        public KylinUploader(IEnumerable<UploadContext> context, string startBoot, string startReplace)
+        {
+            _uploadContext = context;
+
+            Save();
+        }
+
+        /// <summary>
+        /// 上传后文件路径开始根路径
+        /// </summary>
+        private string _startBoot = null;
+
+        /// <summary>
+        /// 上传后文件路径开始根路径需替换的路径
+        /// </summary>
+        private string _startReplace = null;
+
+        /// <summary>
+        /// 保存文件
+        /// </summary>
+        private async void Save()
+        {
+            var results = await _uploadContext.Save();
+
+            results.ConvertUploadResult(_startBoot, _startReplace);
+
+            this.Results = results;
+        }
+
+        /// <summary>
+        /// 待上传的文件上下文集合
+        /// </summary>
+        private IEnumerable<UploadContext> _uploadContext = null;
+
+        /// <summary>
+        /// 第一个上传结果，如果不存在时，返回默认值
+        /// </summary>
+        public UploadResult Result
+        {
+            get
+            {
+                return null != Results ? Results.FirstOrDefault() : null;
+            }
+        }
+
+        /// <summary>
+        /// 返回上传后的结果
+        /// </summary>
+        public List<UploadResult> Results { get; private set; }
+    }
+}
