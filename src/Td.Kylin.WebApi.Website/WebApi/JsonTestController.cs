d81d0d9f626bf8ad27aca71855d42a399b777beb
伍朝辉 <337883612@qq.com>
2016-01-10 15:18:50 +0800

    优化BaseCotroller中对Nullable<Int64>的Json字符串输出

---
+++ src/Td.Kylin.WebApi.Website/WebApi/JsonTestController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Td.Kylin.WebApi.Website.WebApi
+{
+    [Route("api/jsontest")]
+    public class JsonTestController : BaseController
+    {
+        [HttpGet("showlong")]
+        public IActionResult Int64()
+        {
+            TestModel model = new TestModel
+            {
+                Name = "测试",
+                ItemID = long.MaxValue
+            };
+
+            return KylinOk(model);
+        }
+
+        [HttpGet("shownullablelong")]
+        public IActionResult NullableInt64()
+        {
+            TestNullableModel model = new TestNullableModel
+            {
+                Name = "测试",
+                ItemID = long.MaxValue
+            };
+
+            return KylinOk(model);
+        }
+
+        public class TestModel
+        {
+            public string Name { get; set; }
+
+            public long ItemID { get; set; }
+        }
+
+        public class TestNullableModel
+        {
+            public string Name { get; set; }
+
+            public long? ItemID { get; set; }
+        }
+    }
+}
