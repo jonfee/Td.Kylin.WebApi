49db503b4cde05f9e7b773f4a3721bf10ff8c327
袁坷 <562573053@qq.com>
2016-03-23 20:23:08 +0800

    上下文增加区域、经纬度字段

--- src/Td.Kylin.WebApi/BaseController.cs
+++ src/Td.Kylin.WebApi/BaseController.cs
@@ -1,14 +1,52 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
 using Newtonsoft.Json;
 using Td.Kylin.WebApi.Json;
 
 namespace Td.Kylin.WebApi
 {
+    public class RequestParameter
+    {
+        public int LBSArea
+        {
+            get;
+            internal set;
+
+        }
+
+        public double LBSLongitude
+        {
+            get;
+            internal set;
+
+        }
+
+        public double LBSLatitude
+        {
+            get;
+            internal set;
+        }
+    }
+
     /// <summary>
     /// Kylin接口控制器基类
     /// </summary>
     public class BaseController : Controller
     {
+        private static RequestParameter _parameter = new RequestParameter();
+
+        protected RequestParameter Parameter
+        {
+            get
+            {
+                _parameter.LBSArea = int.Parse(HttpContext.Items["LBSArea"].ToString());
+                _parameter.LBSLongitude = double.Parse(HttpContext.Items["LBSLongitude"].ToString());
+                _parameter.LBSLatitude = double.Parse(HttpContext.Items["LBSLatitude"].ToString());
+
+                return _parameter;
+            }
+        }
+
         /// <summary>
         /// Kylin使用的HttpOkObjectResult结果返回
         /// </summary>
