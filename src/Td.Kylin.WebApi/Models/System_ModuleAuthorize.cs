9a39f81977724261d50ecba66e1fb1ec4a81f973
wen15813895276 <313959461@qq.com>
2016-07-05 10:46:09 +0800

    修复model表命

--- src/Td.Kylin.WebApi/Models/System_ModuleAuthorize.cs
+++ src/Td.Kylin.WebApi/Models/System_ModuleAuthorize.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Td.Kylin.WebApi.Models
 {
+	[Table("System_ModuleAuthorize")]
     public class System_ModuleAuthorize
     {
         /// <summary>
